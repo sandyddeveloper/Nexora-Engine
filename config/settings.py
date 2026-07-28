f0e65e42f8f5c90430ad8add01972fd68505daf5
sandyddeveloper <sandyddeveloper@gmail.com>
2026-07-28 21:31:45 +0530

    Fix default values for DEBUG and ALLOWED_HOSTS in settings; add unit tests for environment variable handling

--- config/settings.py
+++ config/settings.py
@@ -146,9 +146,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = os.getenv("LANGUAGE_CODE")
+LANGUAGE_CODE = os.getenv("LANGUAGE_CODE", "en-us")
 
-TIME_ZONE = os.getenv("TIME_ZONE")
+TIME_ZONE = os.getenv("TIME_ZONE", "UTC")
 
 USE_I18N = True
 
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -6,9 +6,14 @@ from unittest.mock import patch
 
 
 class SettingsDefaultsTests(unittest.TestCase):
-    def test_missing_csv_env_vars_default_to_empty_lists(self):
+    def test_missing_optional_env_vars_default_safely(self):
         with patch.dict(os.environ, {}, clear=False):
-            for key in ["ALLOWED_HOSTS", "CORS_ALLOWED_ORIGINS"]:
+            for key in [
+                "ALLOWED_HOSTS",
+                "CORS_ALLOWED_ORIGINS",
+                "LANGUAGE_CODE",
+                "TIME_ZONE",
+            ]:
                 os.environ.pop(key, None)
 
             os.environ.setdefault("SECRET_KEY", "test-secret-key")
@@ -21,6 +26,8 @@ class SettingsDefaultsTests(unittest.TestCase):
 
             self.assertEqual(reloaded_settings.ALLOWED_HOSTS, [])
             self.assertEqual(reloaded_settings.CORS_ALLOWED_ORIGINS, [])
+            self.assertEqual(reloaded_settings.LANGUAGE_CODE, "en-us")
+            self.assertEqual(reloaded_settings.TIME_ZONE, "UTC")
 
 
 if __name__ == "__main__":
