249a2a7afa716215739cc837169ad63ded019f54
sandyddeveloper <sandyddeveloper@gmail.com>
2026-07-29 10:51:30 +0000

    deply detup- inprogress

--- config/settings.py
+++ config/settings.py
@@ -215,7 +215,7 @@ ENVIRONMENT = os.getenv("ENVIRONMENT", "development" if DEBUG else "production")
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
