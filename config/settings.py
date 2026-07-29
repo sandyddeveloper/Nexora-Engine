f14a8b9081577faa29a4526dc56a73a32b60b018
sandyddeveloper <sandyddeveloper@gmail.com>
2026-07-29 17:45:08 +0530

    deply setup

--- config/settings.py
+++ config/settings.py
@@ -228,9 +228,20 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 CELERY_BROKER_URL = os.getenv(
-    "CELERY_BROKER_URL", "amqp://guest:guest@localhost:5672//"
+    "CELERY_BROKER_URL",
+    "redis://nexora-redis:6379/0",
 )
-CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "rpc://")
+
+CELERY_RESULT_BACKEND = os.getenv(
+    "CELERY_RESULT_BACKEND",
+    "redis://nexora-redis:6379/1",
+)
+
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_ENABLE_UTC = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
