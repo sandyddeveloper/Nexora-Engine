685cb6d43c8bd15df312b2fd7b0a8416559739a6
sandyddeveloper <sandyddeveloper@gmail.com>
2026-07-30 11:40:37 +0000

    setuo for deply

--- config/settings.py
+++ config/settings.py
@@ -249,3 +249,4 @@ CELERY_ENABLE_UTC = True
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
