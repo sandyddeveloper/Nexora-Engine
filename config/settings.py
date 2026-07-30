0bad2fbad5675b957e19a1d45a0cb4b36636b31e
sandyddeveloper <sandyddeveloper@gmail.com>
2026-07-30 17:20:30 +0530

    Enhance production security settings in Django configuration

--- config/settings.py
+++ config/settings.py
@@ -249,4 +249,20 @@ CELERY_ENABLE_UTC = True
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+# ==========================
+# Production Security
+# ==========================
+
+if not DEBUG:
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+    SECURE_SSL_REDIRECT = True
+
+    SESSION_COOKIE_SECURE = True
+
+    CSRF_COOKIE_SECURE = True
+
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
